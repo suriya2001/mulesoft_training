057d35fbb46a3e427a7cf9bdfb204f1ca3db8ea3
suriya2001 <60141104+suriya2001@users.noreply.github.com>
2022-03-02 00:04:36 +0530

    Add files via upload

---
+++ code/table.java
@@ -0,0 +1,47 @@
+package net.sqlitetutorial;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class table {
+
+    /**
+     * Create a new table in the test database
+     *
+     */
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/movies.db";
+        
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS Movie (\n"
+                + "	Movie_name text,\n"
+                + "	actor text,\n"
+                + "	actress text,\n"
+		+ "	year int,\n"
+		+ "	director text\n"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        createNewTable();
+    }
+
+}
